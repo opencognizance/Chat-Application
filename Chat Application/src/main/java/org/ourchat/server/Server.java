b584158123b86daa2caeb8c9da174f72731cc26b
Abhishek Das <abhishek.das.opensource@gmail.com>
2024-02-23 01:04:54 +0530

    "Server.java"

--- Chat Application/src/main/java/org/ourchat/server/Server.java
+++ Chat Application/src/main/java/org/ourchat/server/Server.java
@@ -1,14 +1,23 @@
 package org.ourchat.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server implements Runnable{
 
     @Override
     public void run() {
-
+        try {
+            ServerSocket socket = new ServerSocket(9999);
+            Socket client = socket.accept();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     class ConnectionHandler implements Runnable{
-
+        private
         @Override
         public void run() {
 
